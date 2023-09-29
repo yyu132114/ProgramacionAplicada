4916785982152b70e5601211ccf2d12bec74f448
yyu132114 <142937773+yyu132114@users.noreply.github.com>
2023-09-28 23:55:44 -0500

    Update and rename For to if.py

---
+++ primerosProgramas/if.py
@@ -0,0 +1,9 @@
+numero = int(input("Ingresa un número: "))
+
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es igual a cero.")
+
